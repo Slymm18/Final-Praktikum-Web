2bec3e13231aa5c1fbed67358fb13990ebac92f0
mursalim0609@gmail.com <mursalim0609@gmail.com>
2025-12-09 04:09:48 +0800

    perubahan 8

--- auth/proses_register.php
+++ auth/proses_register.php
@@ -7,6 +7,6 @@ $password = $_POST['password']; // sesuai permintaan kamu: tanpa hash
 $query = "INSERT INTO users (username, password, role) VALUES ('$username', '$password', 'user')";
 mysqli_query($connect, $query);
 
-header("Location: index.php?registered=success");
+header("Location: ../index.php?registered=success");
 exit;
 ?>
